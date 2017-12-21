78e4b8b28a56b127cd205b09429d8ba05ca9167f
dychangfeng <dychangfeng@hotmail.com>
2017-12-20 17:00:43 -0700

    add __init__

---
+++ modules/__init__.py
@@ -0,0 +1,12 @@
+"""package Description
+Copyright (c) 2017 Yun Ding <u0674686@utah.edu>
+This code is free software; you can redistribute it and/or modify it
+under the terms of the BSD License (see the file COPYING included with
+the distribution).
+@status:  experimental
+@author:  Yun Ding
+@contact: u0674686@utah.edu
+This package look for g4s in a genome or a list of genomes, and determine
+where are the g4s in terms of genomic features"""
+
+__all__=['clean_up_files','gff_g4_functions', 'g4_genome']
